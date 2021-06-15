4dbc7a5befd247baf5bc066af205bd9c052cfac6
dorfohm <dorfohm@hotmail.com>
2021-06-15 17:35:58 -0300

    curso infinito

--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -1,8 +1,12 @@
+import { Observable } from 'rxjs';
 import {Component, Inject, OnInit} from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from "@angular/material/dialog";
 import {Course} from "../model/course";
 import {FormBuilder, Validators, FormGroup} from "@angular/forms";
 import { CoursesService } from 'app/services/courses.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorage } from '@angular/fire/storage';
+import { concatMap, last } from 'rxjs/operators';
 
 
 @Component({
@@ -15,12 +19,15 @@ export class CourseDialogComponent implements OnInit {
     form: FormGroup;
     description:string;
     course: Course;
+    uploadPercent$: Observable<number>;
+    downloadUrl$: Observable<string>;
 
     constructor(
         private fb: FormBuilder,
         private dialogRef: MatDialogRef<CourseDialogComponent>,
         @Inject(MAT_DIALOG_DATA) course:Course,
-        private coursesService: CoursesService) {
+        private coursesService: CoursesService,
+        private storage:AngularFireStorage) {
 
         this.course = course;
 
@@ -48,6 +55,23 @@ export class CourseDialogComponent implements OnInit {
 
     close() {
         this.dialogRef.close();
+    }
+
+    uploadFile(event){
+      const file: File = event.target.files(0);
+      const filePath = `courses/${this.course.id}/${file.name}`;
+      const task = this.storage.upload(filePath, file);
+      this.uploadPercent$ = task.percentageChanges();
+      this.downloadUrl$ = task.snapshotChanges()
+      .pipe(
+        last(),
+        concatMap(()=> this.storage.ref(filePath).getDownloadURL())
+      );
+      const saveUrl$ = this.downloadUrl$
+      .pipe(
+        concatMap(url => this.coursesService.saveCourse(this.course.id, {uploadedImageUrl: url}))
+      )
+      saveUrl$.subscribe(console.log);
     }
 
 }
